8ea11ead6e6034214c44056e3f5f718879682cc5
Pramish K.C <pramishkc066@gmail.com>
2026-05-08 14:31:28 +0545

    feat: add endpoints for medicine function

---
+++ ePharmacy.API/backend/catalog/urls.py
@@ -0,0 +1,36 @@
+from django.urls import path
+from .views import (
+    CategoryListCreateView,
+    CategoryDetailView,
+    ManufacturerListCreateView,
+    ManufacturerDetailView,
+    MedicineListCreateView,
+    MedicineDetailView,
+    MedicineRelationListCreateView,
+    MedicineRelationDetailView,
+)
+
+urlpatterns = [
+    path("categories/", CategoryListCreateView.as_view(), name="category-list"),
+    path("categories/<uuid:pk>/", CategoryDetailView.as_view(), name="category-detail"),
+    path(
+        "manufacturers/", ManufacturerListCreateView.as_view(), name="manufacturer-list"
+    ),
+    path(
+        "manufacturers/<uuid:pk>/",
+        ManufacturerDetailView.as_view(),
+        name="manufacturer-detail",
+    ),
+    path("medicines/", MedicineListCreateView.as_view(), name="medicine-list"),
+    path("medicines/<uuid:pk>/", MedicineDetailView.as_view(), name="medicine-detail"),
+    path(
+        "medicines/<uuid:medicine_id>/relations/",
+        MedicineRelationListCreateView.as_view(),
+        name="medicine-relation-list",
+    ),
+    path(
+        "relations/<uuid:pk>/",
+        MedicineRelationDetailView.as_view(),
+        name="medicine-relation-detail",
+    ),
+]

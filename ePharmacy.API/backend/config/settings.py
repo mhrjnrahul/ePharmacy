d3cd7ad633861e3ae6cd386400ad5ed6028e631f
vold2emort <pramishkc066@gmail.com>
2026-07-14 15:26:16 +0545

    feat(settings.py): add PageNumber pagination

--- ePharmacy.API/backend/config/settings.py
+++ ePharmacy.API/backend/config/settings.py
@@ -143,6 +143,8 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
     ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 5
 }
 
 SPECTACULAR_SETTINGS = {
